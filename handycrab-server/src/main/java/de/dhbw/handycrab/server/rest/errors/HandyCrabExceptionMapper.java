96ea56b27d9df427008f188188b4062b05afed7d
Nico Dreher <Nico.Dreher98@gmail.com>
2020-03-30 16:21:21 +0200

    Added Exception Mapping Added CI for MongoDB

---
+++ handycrab-server/src/main/java/de/dhbw/handycrab/server/rest/errors/HandyCrabExceptionMapper.java
@@ -0,0 +1,32 @@
+package de.dhbw.handycrab.server.rest.errors;
+
+import de.dhbw.handycrab.server.exceptions.HandyCrabException;
+import org.json.JSONObject;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * The HandyCrabExceptionMapper is used to decide what happens if a HandyCrabException is thrown in a RestService
+ *
+ * @author Nico Dreher
+ * @see javax.ws.rs.ext.ExceptionMapper
+ */
+@Provider
+public class HandyCrabExceptionMapper implements ExceptionMapper<HandyCrabException> {
+    /**
+     * Creates the response of the ExceptionMapper
+     *
+     * @param exception the thrown exception
+     *
+     * @return the response to the client
+     *
+     * @see javax.ws.rs.ext.ExceptionMapper#toResponse(Throwable)
+     */
+    @Override
+    public Response toResponse(HandyCrabException exception) {
+        return Response.status(exception.getStatusCode()).entity(new JSONObject().put("errorCode", exception.getErrorCode()).toString()).type(MediaType.APPLICATION_JSON).build();
+    }
+}
