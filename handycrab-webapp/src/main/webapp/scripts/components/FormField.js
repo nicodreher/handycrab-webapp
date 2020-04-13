b00f962001238cec8dea4657b04339467f038dab
SebastianBoehringer <sebastian33.boehringer@gmail.com>
2020-04-13 18:03:23 +0200

    Extracted Component: FormField

---
+++ handycrab-webapp/src/main/webapp/scripts/components/FormField.js
@@ -0,0 +1,17 @@
+import {Col, Form, Row} from "react-bootstrap";
+import React from "react";
+
+export function FormField(props) {
+    const required = props.required !== undefined ? props.required : true;
+    const disabled = props.disabled !== undefined ? props.disabled : false;
+    const type = props.type !== undefined ? props.undefined : "text";
+    return <Form.Group as={Row}>
+        <Form.Label id={props.id + '_label'} htmlFor={props.id} column sm="2">
+            {props.label}
+        </Form.Label>
+        <Col sm="10">
+            <Form.Control type={type} required={required} id={props.id} value={props.value}
+                          onChange={props.onChange} aria-describedby={props.id + '_label'} disabled={disabled}/>
+        </Col>
+    </Form.Group>
+}
