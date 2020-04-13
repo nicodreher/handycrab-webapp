aa6389300e9ecb7f527414c26dd7d45e06ff792b
SebastianBoehringer <sebastian33.boehringer@gmail.com>
2020-04-13 18:02:38 +0200

    Changed *Page to functional Component

--- handycrab-webapp/src/main/webapp/scripts/pages/RegisterPage.js
+++ handycrab-webapp/src/main/webapp/scripts/pages/RegisterPage.js
@@ -1,18 +1,14 @@
-import React from "react"
+import React from "react";
 import {RegisterForm} from "../components/RegisterForm";
 import {Col, Container, Row} from "react-bootstrap";
 
-export class RegisterPage extends React.Component {
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                        <RegisterForm/>
-                    </Col>
-                </Row>
-            </Container>
+export function RegisterPage(props) {
+    return <Container>
+        <Row>
+            <Col>
+                <RegisterForm/>
+            </Col>
+        </Row>
+    </Container>;
 
-        );
-    }
 }
--- handycrab-webapp/src/main/webapp/scripts/pages/SearchPage.js
+++ handycrab-webapp/src/main/webapp/scripts/pages/SearchPage.js
@@ -1,18 +1,13 @@
-import React from "react"
+import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import {SearchForm} from "../components/SearchForm";
 
-export class SearchPage extends React.Component {
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                        <SearchForm/>
-                    </Col>
-                </Row>
-            </Container>
-
-        );
-    }
+export function SearchPage(props) {
+    return <Container>
+        <Row>
+            <Col>
+                <SearchForm/>
+            </Col>
+        </Row>
+    </Container>;
 }
