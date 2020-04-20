727923d7fea62325ccd08d8e681e63957c55cc65
Nico Dreher <nico.dreher98@gmail.com>
2020-04-20 17:22:02 +0200

    Update NameAlreadyUsedException.java

--- handycrab-server/src/main/java/de/dhbw/handycrab/server/exceptions/NameAlreadyUsedException.java
+++ handycrab-server/src/main/java/de/dhbw/handycrab/server/exceptions/NameAlreadyUsedException.java
@@ -2,6 +2,6 @@ package de.dhbw.handycrab.server.exceptions;
 
 public class NameAlreadyUsedException extends HandyCrabException {
     public NameAlreadyUsedException() {
-        super(400, 5);
+        super(400, 4);
     }
 }
