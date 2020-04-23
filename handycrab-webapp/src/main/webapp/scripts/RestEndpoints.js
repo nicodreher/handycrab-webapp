a78fcccb48e03b579fbc5ca5071f140bbd11c01e
SebastianBoehringer <sebastian33.boehringer@gmail.com>
2020-04-23 21:19:40 +0200

    Extracted url constants into own file

---
+++ handycrab-webapp/src/main/webapp/scripts/RestEndpoints.js
@@ -0,0 +1,12 @@
+const baseUrl = 'http://handycrab.nico-dreher.de/rest'
+
+export const loginUrl = baseUrl + '/users/login';
+export const registerUrl = baseUrl + '/users/register';
+export const logoutUrl = baseUrl + '/users/logout';
+export const getUsernameUrl = baseUrl + '/users/name';
+export const getBarrierUrl = baseUrl + '/barriers/get';
+export const addBarrierUrl = baseUrl + '/barriers/add';
+export const modifyBarrierUrl = baseUrl + '/barriers/modify';
+export const voteForBarrierUrl = baseUrl + '/barriers/vote';
+export const addSolutionUrl = baseUrl + '/barriers/solution';
+export const voteForSolutionUrl = baseUrl + '/barriers/solutions/vote';
